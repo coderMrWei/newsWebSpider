b297157b3eaca23bb52b73f270e0ec298e7e2db9
yibao.wei <yibao.wei@qunar.com>
2018-09-09 10:55:58 +0800

    修改

---
+++ newsSpider/src/main/java/com/merphy/dao/NewsDao.java
@@ -0,0 +1,14 @@
+package com.merphy.dao;
+
+import com.merphy.dao.model.News;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * @author yibaowei on 9/9/18.
+ */
+public interface NewsDao {
+    News getNewsById(Integer id);
+    List<News> queryNews(Map<String,Object> queryParam);
+    Long getTotal(Map<String,Object> queryParam);
+}
--- newsSpider/src/main/sql/news.sql
+++ newsSpider/src/main/sql/news.sql
@@ -1,15 +1,15 @@
 DROP TABLE  IF EXISTS news;
 CREATE TABLE news(
-  id INT PRIMARY KEY not NULL COMMIT '主键id',
-  title VARCHAR(256) not NULL COMMIT '标题',
-  title_image VARCHAR(1024) COMMIT '新闻标题图片',
-  content text NOT NULL COMMIT '新闻内容',
-  tag VARCHAR(256) COMMIT '新闻标签',
-  source_website VARCHAR(256) COMMIT '新闻来源网站名称',
-  source_website_url VARCHAR(256) COMMIT '新闻来源网站网址',
-  source_url VARCHAR(256) comment '新闻网址',
-  visit_count BIGINT COMMIT '浏览次数',
-  comment_count INT COMMIT '点评次数',
-  create_time TIMESTAMP  COMMIT '创建时间',
-  update_time TIMESTAMP  COMMIT '更新时间'
-)engine=innoDB DEFAULT charset utf8 comment='新闻表'
+  id INT PRIMARY KEY not NULL COMMENT '主键id',
+  title VARCHAR(256) not NULL COMMENT '标题',
+  title_image VARCHAR(1024) COMMENT '新闻标题图片',
+  content text NOT NULL COMMENT '新闻内容',
+  tag VARCHAR(256) COMMENT '新闻标签',
+  source_website VARCHAR(256) COMMENT '新闻来源网站名称',
+  source_website_url VARCHAR(256) COMMENT '新闻来源网站网址',
+  source_url VARCHAR(256) COMMENT '新闻网址',
+  visit_count BIGINT COMMENT '浏览次数',
+  comment_count INT COMMENT '点评次数',
+  create_time TIMESTAMP COMMENT '创建时间',
+  update_time TIMESTAMP COMMENT '更新时间',
+  about TEXT COMMENT '备注')engine=innoDB DEFAULT charset utf8 comment='新闻表';
